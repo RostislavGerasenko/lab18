7f2644d1b177809a3af96d390e07fa1be3458be8
RostislavGerasenko <91475179+RostislavGerasenko@users.noreply.github.com>
2021-09-27 17:21:50 +0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,6 @@
+N = int(input("Size of the arrays: "))
+arr1 = [int(input(f"Element number {i+1} of the array: ")) for i in range(N)]
+arr2 = [int(input(f"Element number {i+1} of the array: ")) for i in range(N)]
+for i in range(N):
+    arr1[i], arr2[i] = arr2[i], arr1[i]
+print(f"Array A: {arr1}, Array B: {arr2}")
