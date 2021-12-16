713b56720d056eb0f60ce2b90d61ada01eb0f961
RostislavGerasenko <91475179+RostislavGerasenko@users.noreply.github.com>
2021-12-16 11:28:14 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,6 +1,7 @@
+import bisect
+
 N = int(input("Size of the array: "))
 arr = [int(input(f"Element number {i+1} of the array: ")) for i in range(N)]
-min_value = min(arr)
-arr.remove(min_value)
-arr.insert(0, min_value)
+first_value = arr.pop(0)
+bisect.insort_left(arr, first_value)
 print(f"Sorted array: {arr}")
